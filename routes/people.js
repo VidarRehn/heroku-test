56e41fb7dcbfb08ec2008034f4cd586bbcb9edb0
Vidar Rehn <vidar.rehn@gmail.com>
2022-05-13 23:22:35 +0200

    added post, put and delete routes

--- routes/people.js
+++ routes/people.js
@@ -16,8 +16,45 @@ router.post('/', (req, res) => {
     
     person.save().then(person => {
         res.send(person)
-    }).catch(error => {
-        res.status(500).send('Error: ', error)
+    })
+})
+
+// GET all
+
+router.get('/', (req, res) => {
+    Person.find().then(people => {
+        res.send(people)
+    })
+})
+
+
+// GET by id
+
+router.get('/:id', (req, res) => {
+    Person.findById(req.params.id).then(person => {
+        res.send(person)
+    })
+})
+
+// UPDATE
+
+router.put('/:id', (req, res) => {
+    const updatedPerson = Person.findByIdAndUpdate(req.params.id, {
+        firstName: req.body.personFirstName,
+        lastName: req.body.personLastName,
+        age: req.body.personAge
+    },{
+        new: true
+    }).then(person => {
+        res.send(person)
+    })
+})
+
+// DELETE
+
+router.delete('/:id', (req, res) => {
+    Person.findByIdAndRemove(req.params.id).then(person => {
+        res.send(person)
     })
 })
 
