a4a93c3488dcba27890dcce28e94f96bd6040efe
Vidar Rehn <vidar.rehn@gmail.com>
2022-05-14 17:28:47 +0200

    added a submit form

--- main.js
+++ main.js
@@ -6,3 +6,27 @@ const getData = async () => {
 }
 
 getData()
+
+const firstNameInput = document.querySelector('#first-name')
+const lastNameInput = document.querySelector('#last-name')
+const ageInput = document.querySelector('#age')
+const myForm = document.querySelector('#my-form')
+
+
+const addNewPerson = async () => {
+    await fetch('https://murmuring-castle-51611.herokuapp.com/api/people', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            firstName: firstNameInput.value,
+            lastName: lastNameInput.value,
+            age: ageInput.value
+        })
+    })
+}
+
+myForm.addEventListener('submit', () => {
+    addNewPerson()
+})
